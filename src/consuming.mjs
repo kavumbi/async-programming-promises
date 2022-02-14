47b2891751ccba729ca3d90b5aaf8199dc287b5b
Ami-Eugène Samba <ami_eugene@outlook.com>
2022-02-14 11:40:07 +0100

    Final update

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -35,4 +35,19 @@ export function chainCatch(){
        .catch(err => setText(err));
 }
 export function final(){
+    showWaiting();
+    axios.get("http://localhost:3000/orders/1").then(({data}) => {
+        return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`)
+     })
+     .then(({data}) => {
+        setText(JSON.stringify(data.my.city));
+       })
+       .catch(err => setText(err))
+       .finally(() => {
+           setTimeout(()=>{
+               hideWaiting();
+           }, 1500)
+
+           appendText(" -- Completely Done")
+       });    
 }
